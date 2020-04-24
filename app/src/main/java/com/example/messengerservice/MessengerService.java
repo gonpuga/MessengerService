40135ca361d259cf83fa5f555ec40e6c6928d3d7
gonpuga <gonpuga@gmail.com>
2020-04-24 12:32:29 +0200

    Bound Service with Messenger

--- app/src/main/java/com/example/messengerservice/MessengerService.java
+++ app/src/main/java/com/example/messengerservice/MessengerService.java
@@ -2,6 +2,7 @@ package com.example.messengerservice;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -15,33 +16,24 @@ import android.widget.Chronometer;
 import java.lang.ref.WeakReference;
 
 public class MessengerService extends Service {
-    private static String LOG_TAG = "MessengerService";
+    private static String TAG = "MessengerService";
     private Chronometer mChronometer;
     static final int MSG_GET_TIMESTAMP = 1000;
 
-    static class BoundServiceHandler extends Handler {
+    class BoundServiceHandler extends Handler {
         private final WeakReference<MessengerService> mService;
 
         public BoundServiceHandler(MessengerService service) {
             mService = new WeakReference<MessengerService>(service);
         }
 
-        @Override
-        public void handleMessage(Message msg) {
+        @Override public void handleMessage(Message msg) {
             switch (msg.what) {
                 case MSG_GET_TIMESTAMP:
-                    long elapsedMillis = SystemClock.elapsedRealtime()
-                            - mService.get().mChronometer.getBase();
-                    int hours = (int) (elapsedMillis / 3600000);
-                    int minutes = (int) (elapsedMillis - hours * 3600000) / 60000;
-                    int seconds = (int) (elapsedMillis - hours * 3600000 - minutes * 60000) / 1000;
-                    int millis = (int) (elapsedMillis - hours * 3600000 - minutes
-                            * 60000 - seconds * 1000);
                     Messenger activityMessenger = msg.replyTo;
                     Bundle b = new Bundle();
-                    b.putString("timestamp",
-                            String.format("%02d:%02d:%02d:%03d", hours, minutes, seconds, millis));
-                    Message replyMsg = Message.obtain(null, MSG_GET_TIMESTAMP);
+                    b.putString("timestamp", getTimestamp());
+                    Message replyMsg= Message.obtain(null, MSG_GET_TIMESTAMP);
                     replyMsg.setData(b);
                     try {
                         activityMessenger.send(replyMsg);
@@ -57,37 +49,45 @@ public class MessengerService extends Service {
 
     final Messenger mMessenger = new Messenger(new BoundServiceHandler(this));
 
-    @Override
-    public void onCreate() {
+    @Override public void onCreate() {
         super.onCreate();
-        Log.v(LOG_TAG, "in onCreate");
+        Log.v(TAG, "in onCreate");
         mChronometer = new Chronometer(this);
         mChronometer.setBase(SystemClock.elapsedRealtime());
         mChronometer.start();
     }
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        Log.v(LOG_TAG, "in onBind");
+    @Override public IBinder onBind(Intent intent) {
+        Log.v(TAG, "in onBind");
         return mMessenger.getBinder();
     }
 
-    @Override
-    public void onRebind(Intent intent) {
-        Log.v(LOG_TAG, "in onRebind");
+    @Override public void onRebind(Intent intent) {
+        Log.v(TAG, "in onRebind");
         super.onRebind(intent);
     }
 
-    @Override
-    public boolean onUnbind(Intent intent) {
-        Log.v(LOG_TAG, "in onUnbind");
+    @Override public boolean onUnbind(Intent intent) {
+        Log.v(TAG, "in onUnbind");
         return true;
     }
 
-    @Override
-    public void onDestroy() {
+    @Override public void onDestroy() {
         super.onDestroy();
-        Log.v(LOG_TAG, "in onDestroy");
+        Log.v(TAG, "in onDestroy");
         mChronometer.stop();
+    }
+
+    public String getTimestamp() {
+        long elapsedMillis = SystemClock.elapsedRealtime()
+                - mChronometer.getBase();
+        int hours = (int) (elapsedMillis / 3600000);
+        int minutes = (int) (elapsedMillis - hours * 3600000) / 60000;
+        int seconds = (int) (elapsedMillis - hours * 3600000 - minutes *
+                60000) / 1000;
+        int millis = (int) (elapsedMillis - hours * 3600000 - minutes *
+                60000 - seconds * 1000);
+        return String.format("%02d:%02d:%02d:%03d", hours, minutes,
+                seconds, millis);
     }
 }
